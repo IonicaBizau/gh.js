fb23f6ad0def3e97dfe81a02a65e2679cb33d222
Ionică Bizău <bizauionica@gmail.com>
2015-09-15 17:43:31 +0300

    Comments

--- example/index.js
+++ example/index.js
@@ -1,5 +1,7 @@
+// Dependencies
 var GitHub = require("../lib");
 
+// Create a new instance
 var gh = new GitHub();
 gh.get("users/IonicaBizau", function (err, repos) {
     console.log(err || repos);
